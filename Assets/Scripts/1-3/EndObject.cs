1b0cd6a6492e7152d3cb8c5ef7ec5a868e33d002
Blady <michel_dijkstra1@hotmail.com>
2016-03-16 13:44:51 +0100

    Finished level 3.

--- Assets/Scripts/1-3/EndObject.cs
+++ Assets/Scripts/1-3/EndObject.cs
@@ -4,12 +4,26 @@ using System.Collections;
 public class EndObject : MonoBehaviour 
 {
     int total = 0;
+    int points;
+    GameController gameController;
+
+    void Start()
+    {
+        gameController = GameObject.Find("GameController").GetComponent<GameController>();
+    }
 
     void Update()
     {
         if (total == 3)
         {
-
+            gameController.totalScore = points;
+            gameController.doneWithMiniGame = true;
+            gameController.won = true;
+            GameObject[] humans = GameObject.FindGameObjectsWithTag("BlueHuman");
+            foreach (GameObject temp in humans)
+            {
+                temp.SetActive(false);
+            }
         }
     }
 
